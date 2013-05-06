6cb64fec44d1ed85bbb96e2178918fbac9b1540f
tworker <tworker@thoughtworks.com>
2013-05-06 15:11:40 -0700

    Ensure that Loaders::Base is loaded before everything else.

--- lib/mingle_macro_development_toolkit/loaders.rb
+++ lib/mingle_macro_development_toolkit/loaders.rb
@@ -1,5 +1,9 @@
 #Copyright 2007 - 2013 ThoughtWorks, Inc.  All rights reserved.
 
+Dir.glob(File.join(File.dirname(__FILE__), "loaders", "**/base.rb")).each do |file|
+  require file
+end
+
 Dir.glob(File.join(File.dirname(__FILE__), "loaders", "**/*.rb")).each do |file|
   require file
 end
